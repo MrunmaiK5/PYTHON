9ff8464bc0ebf0494e8a1a6b1eb149d7622edffc
MrunmaiK5 <mrunmai0205@gmail.com>
2026-01-25 00:15:01 +0530

    Added Assignment__17

--- Assignments/Assignment__17/Arithmetic.py
+++ Assignments/Assignment__17/Arithmetic.py
@@ -1,6 +1,7 @@
 #######################################################################################
-#   Function name   :   Add
-#   Description     :   Display addition of 2 numbers
+#   Moduke name     :   Arithmetic.py
+#   Description     :   Contains four functions Add(), Sub(), Mult(), Div() 
+#                       to perform addition, subtraction, multiplication, division 
 #   Input           :   Integer, Integer
 #   Output          :   Integer
 #   Author          :   Mrunmai Jitendra Khadpe
@@ -10,38 +11,11 @@
 def Add(No1, No2):
     return No1 + No2
 
-#######################################################################################
-#   Function name   :   Sub
-#   Description     :   Display subtraction of 2 numbers
-#   Input           :   Integer, Integer
-#   Output          :   Integer
-#   Author          :   Mrunmai Jitendra Khadpe
-#   Date            :   22/01/26
-#######################################################################################
-
 def Sub(No1, No2):
     return No1 - No2
 
-#######################################################################################
-#   Function name   :   Mult
-#   Description     :   Display Multiplication of 2 numbers
-#   Input           :   Integer, Integer
-#   Output          :   Integer
-#   Author          :   Mrunmai Jitendra Khadpe
-#   Date            :   22/01/26
-#######################################################################################
-
 def Mult(No1, No2):
     return No1 * No2
-
-#######################################################################################
-#   Function name   :   Div
-#   Description     :   Display division of 2 numbers
-#   Input           :   Integer, Integer
-#   Output          :   Integer
-#   Author          :   Mrunmai Jitendra Khadpe
-#   Date            :   22/01/26
-#######################################################################################
 
 def Div(No1, No2):
     return No1 / No2
